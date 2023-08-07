ff027e858d6e34304e2ef163c2e0109512bdfa3b
TanHan Lee <lth9312@gmail.com>
2023-08-07 22:43:42 +0900

    logback 생성

--- MSA-UI/src/main/java/com/msa/ui/UIController.java
+++ MSA-UI/src/main/java/com/msa/ui/UIController.java
@@ -1,5 +1,7 @@
 package com.msa.ui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,23 +9,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class UIController {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping(value = {"/", "/index"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String index() {
+        logger.debug("request : index");
         return "index";
     }
 
     @RequestMapping(value = "/contact", method = {RequestMethod.GET, RequestMethod.POST})
     public String contact() {
+        logger.debug("request : contact");
         return "contact";
     }
 
     @RequestMapping(value = "/projects", method = {RequestMethod.GET, RequestMethod.POST})
     public String projects() {
+        logger.debug("request : projects");
         return "projects";
     }
 
     @RequestMapping(value = "/resume", method = {RequestMethod.GET, RequestMethod.POST})
     public String resume() {
+        logger.debug("request : resume");
         return "resume";
     }
 }
