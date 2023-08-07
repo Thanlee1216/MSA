a53a74988f111162516e7b3698730793480545d5
TanHan Lee <lth9312@gmail.com>
2023-08-07 22:32:02 +0900

    부트스트랩 탬플릿 적용

--- MSA-UI/src/main/java/com/msa/ui/UIController.java
+++ MSA-UI/src/main/java/com/msa/ui/UIController.java
@@ -7,8 +7,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class UIController {
 
-    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value = {"/", "/index"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/contact", method = {RequestMethod.GET, RequestMethod.POST})
+    public String contact() {
+        return "contact";
+    }
+
+    @RequestMapping(value = "/projects", method = {RequestMethod.GET, RequestMethod.POST})
+    public String projects() {
+        return "projects";
+    }
+
+    @RequestMapping(value = "/resume", method = {RequestMethod.GET, RequestMethod.POST})
+    public String resume() {
+        return "resume";
     }
 }
